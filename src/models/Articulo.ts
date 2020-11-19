7ca3e535e6d488e5ed0d0687c32d1b3d50360e3d
Daniel <danielcdc8591@gmail.com>
2020-11-19 14:19:51 +0100

    Añadido model e implementando el componente

---
+++ src/models/Articulo.ts
@@ -0,0 +1,20 @@
+class Articulo {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    modelo: string;
+    cantidad: number;
+    precioOriginal: number;
+    precioRebajado: number;
+
+    constructor(id, nombre, descripcion, modelo, cantidad = 1, precioOriginal){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.modelo = modelo;
+        this.cantidad = cantidad;
+        this.precioOriginal = precioOriginal;
+    }
+}
+
+let lista_articulos: Articulo[] = new Array();
